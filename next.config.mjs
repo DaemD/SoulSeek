f1ef94d208586c890732fe9f6b1003c14fe4e6af
Daeem Azeem <daeemazeemdean@gmail.com>
2025-03-13 11:46:26 +0500

    updated next config. removed environemnts

--- next.config.mjs
+++ next.config.mjs
@@ -17,10 +17,10 @@ const nextConfig = {
     unoptimized: true,
   },
   experimental: {
-    webpackBuildWorker: true,
-    parallelServerBuildTraces: true,
-    parallelServerCompiles: true,
-    appDir: true, // If using App Router
+  //   webpackBuildWorker: true,
+  //   parallelServerBuildTraces: true,
+  //   parallelServerCompiles: true,
+  //   appDir: true, // If using App Router
   },
 }
 
